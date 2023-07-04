d9a6aa3be283da46c93a1e76109f3c09ecbf288f
hojun <ghwnsdl999@naver.com>
2023-07-04 15:30:02 +0900

    [FE] add : 기본 레이아웃 설정 & 헤더 컴포넌트 제작

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "../components/Header";
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
 
@@ -15,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={openSans.className}>
-      <body>{children}</body>
+      <body className="max-w-screen-lg mx-auto">
+        <Header />
+        <hr />
+        {children}
+      </body>
     </html>
   );
 }
